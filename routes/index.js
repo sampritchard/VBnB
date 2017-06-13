2fbffda0fd3c82cb167b967035bf58e8b84d4982
Kavita <kkavita92@gmail.com>
2017-06-13 16:14:36 +0100

    Merge two directories

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+addresses = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'VeBnB' });
+});
+
+router.get('/spaces', function(req, res) {
+	res.render('spaces', { title: 'Listings', addresses: addresses});
+});
+
+router.post('/spaces', function(req, res) {
+	var address = req.body.address;
+	addresses.push(address);
+	res.redirect('/spaces');
 });
 
 module.exports = router;
--- test/test.js
+++ test/test.js
@@ -1,27 +1,43 @@
 process.env.NODE_ENV = 'test';
 var app = require('../app');
-// use zombie.js as headless browser
 var Browser = require('zombie');
 var http = require('http');
 
-describe('contact page', function() {
+
+describe('VeBnB', function() {
   before(function() {
     this.server = http.createServer(app).listen(3000);
     this.browser = new Browser({ site: 'http://localhost:3000' });
   });
 
-  // load the contact page
-  before(function(done) {
-    this.browser.visit('/', done);
+  describe('home page', function() {
+    before(function(done) {
+      this.browser.visit('/', done);
+    });
+
+    it('should be successful', function() {
+      this.browser.assert.success();
+    });
+
+    it('should have content', function() {
+      this.browser.assert.text('title', 'VeBnB');
+    });
+
   });
 
-  it('should be successful', function() {
-    this.browser.assert.success();
-  });
+    describe('listings page', function() {
+      before(function(done) {
+        this.browser.visit('/spaces', done);
+      });
 
-  it('should have content', function() {
-    this.browser.assert.text('title', 'VeBnB');
-  });
+      it('should be successful', function() {
+        this.browser.assert.success();
+      });
+
+      it('should have content', function() {
+        this.browser.assert.text('body', 'Listings');
+      });
+    });
 
 
 
