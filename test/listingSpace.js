c38c2ac597ddca3ce9c72664c1de98f2a42c3365
RSijelmass <R.Sijelmass@gmail.com>
2017-06-14 18:53:32 +0100

    Create new test for Space model - check if new space is created in database with an address - update package.json not to include chai-change

---
+++ test/listingSpace.js
@@ -0,0 +1,39 @@
+//var assert = require('assert')
+var mongoose = require('mongoose')
+var chai = require('chai')
+//var chaiChange = require('chai-change')
+var Space = require('../models/space').Space;
+var expect = chai.expect;
+//chai.use(chaiChange);
+
+describe('Spaces', function() {
+
+	before((done)=>{
+		mongoose.createConnection('mongodb://localhost/test_2');
+		mongoose.connection
+			.once('open', ()=>{ done(); })
+			.on('error', (error)=> {
+				console.warn('Error', error);
+			});
+	});
+
+	beforeEach((done)=>{
+		mongoose.connection.collections.spaces.drop(()=>{
+			done();
+			});
+	});
+
+	describe('Creating space', ()=>{
+			
+		it('saves a space', (done) => {
+			var space = new Space({address: 'Fake 22'})	
+			space.save()
+				.then(function() {	
+					Space.find({}, function(err, spaces) {
+						expect(spaces[0].address).to.equal('Fake 22')
+						done();
+					});
+				});
+		});
+	});
+});
