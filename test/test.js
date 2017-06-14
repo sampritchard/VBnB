dd1dcb613856c4528147ee153b0f5892fd066784
charliefea <charlotte.feather@yahoo.com>
2017-06-14 11:37:39 +0100

    Testing for US 3 completed

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ var http = require('http');
 
 
 describe('VeBnB', function() {
+
   before(function() {
     this.server = http.createServer(app).listen(8080);
     this.browser = new Browser({ site: 'http://localhost:8080' });
@@ -23,35 +24,43 @@ describe('VeBnB', function() {
       this.browser.assert.text('title', 'VeBnB');
     });
 
+    it('should have an enter button',function() {
+      this.browser.assert.text('button', 'Enter');
+    });
 
-  });
-
-    describe('listings page', function() {
+    it('has a Choose button', function() {
       before(function(done) {
-        this.browser.visit('/spaces', done);
-      });
-
-      it('should be successful', function() {
-        this.browser.assert.success();
-      });
-
-      it('has a request button', function() {
-        this.browser.assert.text('button', 'Request');
-      });
-
-
-
-
-      it('the request button sends to a confirmation page', function() {
-          this.browser.pressButton('Request').then(function(){
-            assert.text('body', 'Your request has been submitted');
-          })
+        this.browser.pressButton('Enter').then(function(){
+          this.browser.assert.text('body', 'Listings')
+        });
+        this.browser.assert.text('button', 'Choose')
       });
     });
 
-	after(function(done) {
-		this.server.close(done);
-	});
+    it('the request button sends to a confirmation page', function() {
+      before(function(done) {
+        this.browser.pressButton('Enter').then(function(){
+          this.browser.assert.text('body', 'Listings')
+        });
 
+        this.browser.pressButton('Choose').then(function(){
+          assert.text('body', 'Your request has been submitted');
+        })
+      });
+    });
+  });
 
+  describe('listings page', function() {
+    before(function(done) {
+      this.browser.visit('/spaces', done);
+    });
+
+    it('should be successful', function() {
+      this.browser.assert.success();
+    });
+  });
+
+  after(function(done) {
+    this.server.close(done);
+  });
 });
