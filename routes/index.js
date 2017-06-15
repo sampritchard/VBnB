91de0d08fa75a43e136299e31b773ad87ba613e2
Sam Pritchard <sampritchard@msn.com>
2017-06-15 16:18:19 +0100

    user cannot put in a negative price for their space

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Space = require("../models/space").Space;
+var User = require("../models/user").User;
 
 
 /* GET home page. */
