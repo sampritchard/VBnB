443d9d32dd4a0db31830d7cf8e3516c83f7a21ef
Kavita <kkavita92@gmail.com>
2017-06-15 10:45:18 +0100

    - Create User model - Write tests for model and validation of inputs

---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username: {
+        type: String,
+        required: true
+      }
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = {
+  User: User
+}
---
+++ test/userNew.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose')
+var chai = require('chai')
+var User = require('../models/user').User;
+var expect = chai.expect;
+
+describe('Users', function() {
+
+  before((done)=>{
+		mongoose.createConnection('mongodb://localhost/test_2');
+		mongoose.connection
+			.once('open', ()=>{ done(); })
+			.on('error', (error)=> {
+				console.warn('Error', error);
+			});
+      done();
+	});
+
+	beforeEach((done)=>{
+		mongoose.connection.collections.users.drop(()=>{
+			done();
+			});
+	});
+
+describe('validating inputs', ()=> {
+  it('validates if username is input', () => {
+    var user = new User();
+    user.save(function(error) {
+      expect.to.equal(error.errors['username'].message,
+      'Path `name` is required.');
+
+      error = user.validateSync();
+      expect.to.equal(error.errors['username'].message,
+        'Path `name` is required.');
+      });
+    });
+
+  });
+
+  describe('new user', ()=>{
+    it('saves new user to user database', (done) => {
+      var user = new User({username: 'Voldemort'});
+      user.save()
+        .then(function() {
+          User.find({}, function(err, users) {
+            expect(users[0].username).to.equal('Voldemort');
+            done();
+          });
+        });
+    });
+  });
+
+});
